221201f87a3c4817a73361d71850f0661ea11171
David Smith <dbsmith.dbs83@gmail.com>
2016-09-18 20:38:38 -0700

    converted album and photo tests to using factoryboy

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -1,23 +1,45 @@
+"""Tests for the Photo and ALbum Models."""
 from django.test import TestCase
 from imager_images.models import Photo, Album
 from imager_profile.models import ImagerProfile
 from django.contrib.auth.models import User
+import factory
 
-# Create your tests here.
+
+#class UserFactory(factory.django.DjangoModelFactory):
+#    class Meta:
+#        model = User
+
+#    username = factory.Sequence(lambda n: "user{}".format(n))
+#    email = factory.Sequence(
+#        lambda n: "user{}@example.com".format(n)
+#    )
+
+
+class PhotoFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Photo
+    title = 'Test Photo'
+
+
+class AlbumFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Album
+    title = 'Test Album'
+
+
 class PhotoTestCase(TestCase):
     def setUp(self):
         self.user = User(username='test_user')
         self.user.save()
         self.profile = ImagerProfile(user=self.user)
         self.profile.save()
-        self.photo = Photo(user=self.user)
-        self.photo.title = 'test title'
+        self.photo = PhotoFactory(user=self.user)
         self.photo.save()
 
     def teardown(self):
         self.user.delete()
         self.profile.delete()
-        self.photo.delete()
 
     def test_user_reference(self):
         """Test photo is associated with correct user."""
@@ -30,19 +52,15 @@ class AlbumTestCase(TestCase):
         self.user.save()
         self.profile = ImagerProfile(user=self.user)
         self.profile.save()
-        self.photo = Photo(user=self.user)
-        self.photo.title = 'test title'
+        self.photo = PhotoFactory(user=self.user)
         self.photo.save()
-        self.album = Album(user=self.user)
-        self.album.title = 'test user album'
-        self.album.cover = self.photo
+        self.album = AlbumFactory(cover=self.photo, user=self.user)
         self.album.save()
         self.album.photos.add(self.photo)
 
     def teardown(self):
         self.user.delete()
         self.profile.delete()
-        self.photo.delete()
 
     def test_album_photos(self):
         """Test album size."""
